a9916f19230ff43a3ba4aa7b6d96a8a6fdeb92c2
1o18z <qkrdmswl1018@naver.com>
2026-05-11 21:59:52 +0900

    refactor: 토너먼트 요청 DTO 파일 분리 및 구현 완성

--- src/main/kotlin/com/depromeet/team3/tournament/controller/dto/AddTournamentItemsRequest.kt
+++ src/main/kotlin/com/depromeet/team3/tournament/controller/dto/AddTournamentItemsRequest.kt
@@ -1,3 +1,16 @@
 package com.depromeet.team3.tournament.controller.dto
 
-data class AddTournamentItemRequest()
+import com.depromeet.team3.tournament.service.dto.AddTournamentItems
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.Size
+
+data class AddTournamentItemsRequest(
+    @field:NotEmpty
+    @field:Size(min = 2)
+    val itemIds: List<Long>,
+) {
+    fun toAddTournamentItems(tournamentId: Long): AddTournamentItems = AddTournamentItems(
+        tournamentId = tournamentId,
+        itemIds = itemIds,
+    )
+}
--- src/main/kotlin/com/depromeet/team3/tournament/controller/dto/CreateTournamentRequest.kt
+++ src/main/kotlin/com/depromeet/team3/tournament/controller/dto/CreateTournamentRequest.kt
@@ -1,3 +1,11 @@
 package com.depromeet.team3.tournament.controller.dto
 
-data class CreateTournamentRequest()
+import com.depromeet.team3.tournament.service.dto.CreateTournament
+import jakarta.validation.constraints.NotBlank
+
+data class CreateTournamentRequest(
+    @field:NotBlank
+    val name: String,
+) {
+    fun toCreateTournament(): CreateTournament = CreateTournament(name = name)
+}
