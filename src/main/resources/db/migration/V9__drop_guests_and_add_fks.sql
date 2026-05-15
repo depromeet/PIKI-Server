6d3f7351467cd61f868d8b687e1fbde8363277b7
sevineleven <psv980817@naver.com>
2026-05-15 15:09:07 +0900

    chore: Flyway 마이그레이션 CHECK 제약 및 ON DELETE 명시 추가
    - V5: identity_type CHECK ('GUEST', 'MEMBER')
    - V6: provider CHECK ('KAKAO', 'GOOGLE', 'APPLE'), fk_user_detail_user ON DELETE RESTRICT
    - V8: role CHECK ('OWNER', 'FRIEND'), fk_tp_tournament ON DELETE CASCADE, fk_tp_user ON DELETE RESTRICT
    - V9: fk_tournament_user ON DELETE RESTRICT

--- src/main/resources/db/migration/V9__drop_guests_and_add_fks.sql
+++ src/main/resources/db/migration/V9__drop_guests_and_add_fks.sql
@@ -1,4 +1,4 @@
 DROP TABLE guests;
 
 ALTER TABLE tournament
-    ADD CONSTRAINT fk_tournament_user FOREIGN KEY (user_id) REFERENCES user (id);
+    ADD CONSTRAINT fk_tournament_user FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE RESTRICT;
