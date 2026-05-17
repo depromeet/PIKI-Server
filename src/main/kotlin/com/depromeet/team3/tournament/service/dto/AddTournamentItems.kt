fd38ffcad4c9b995519c33cb638f5b512033d9e6
1o18z <qkrdmswl1018@naver.com>
2026-05-17 21:51:46 +0900

    refactor: CreateTournament와 AddTournamentItems DTO를 별도 파일로 분리

--- src/main/kotlin/com/depromeet/team3/tournament/service/dto/AddTournamentItems.kt
+++ src/main/kotlin/com/depromeet/team3/tournament/service/dto/AddTournamentItems.kt
@@ -1,9 +1,5 @@
 package com.depromeet.team3.tournament.service.dto
 
-data class CreateTournament(
-    val name: String,
-)
-
 data class AddTournamentItems(
     val tournamentId: Long,
     val itemIds: List<Long>,
