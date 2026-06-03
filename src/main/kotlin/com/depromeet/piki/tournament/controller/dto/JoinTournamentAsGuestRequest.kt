1fc8807f6df32ea6f03ffe113ffcc55e28be3f4b
1o18z <qkrdmswl1018@naver.com>
2026-06-04 00:38:55 +0900

    fix: JoinTournamentAsGuestRequest @NotBlank 한국어 메시지 추가

--- src/main/kotlin/com/depromeet/piki/tournament/controller/dto/JoinTournamentAsGuestRequest.kt
+++ src/main/kotlin/com/depromeet/piki/tournament/controller/dto/JoinTournamentAsGuestRequest.kt
@@ -7,7 +7,7 @@ import jakarta.validation.constraints.Size
 data class JoinTournamentAsGuestRequest(
     @field:Pattern(regexp = "[A-Z]{3}\\d{3}", message = "초대 코드는 영어 대문자 3자리 + 숫자 3자리 형식이어야 합니다.")
     val inviteCode: String?,
-    @field:NotBlank
+    @field:NotBlank(message = "닉네임은 비어 있을 수 없습니다.")
     @field:Size(max = 10, message = "닉네임은 10자 이하여야 합니다.")
     val nickname: String,
 )
